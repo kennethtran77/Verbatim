ee3ac9337fea9c59741a871404811d1d25ab2684
Kenneth Tran <kennethtran77@gmail.com>
2022-08-31 00:51:11 -0400

    Remove unused deps

--- server/src/controllers/conjugation_race.ts
+++ server/src/controllers/conjugation_race.ts
@@ -1,11 +1,8 @@
-import { GlobalServices } from "../services";
-import { ConjugationRaceEvents, ConjugationRaceServices } from "../services/active/conjugation_race";
+import { ConjugationRaceEvents } from "../services/active/conjugation_race";
 import { EventListenerService } from "../services/global/event_listener";
 import { Controller } from "./global";
 
 const useConjugationRaceGame = (
-    globalServices: GlobalServices,
-    conjugationRaceServices: ConjugationRaceServices,
     events: ConjugationRaceEvents
 ): Controller => (eventListener: EventListenerService) => {
     
