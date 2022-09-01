679fd69519942314be7d730e7f4ea1bae431f0a2
Kenneth Tran <kennethtran77@gmail.com>
2022-08-31 23:06:49 -0400

    Use duration interface

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,11 +1,12 @@
 import { Player } from "../../../server/src/models/player";
 import { GameContext } from "./game";
 import Response from "../../../server/src/models/response";
+import { Duration } from "../../../server/src/models/game";
 
 interface IApi {
     getPlayer(): Promise<Response<Player>>;
     getGameStatus(gameCode: string): Promise<Response<string>>;
-    createGame(mode: string, duration: { minutes: number, seconds: number }, tenses: string[], maxPlayers: number): Promise<Response<string>>;
+    createGame(mode: string, duration: Duration, tenses: string[], maxPlayers: number): Promise<Response<string>>;
     joinGame(gameCode: string): Promise<Response<GameContext>>;
     leaveGame(): void;
 }
