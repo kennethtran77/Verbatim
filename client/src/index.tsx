8762d86825d3c6f601859bd624c722cdf7755bba
Kenneth Tran <kennethtran77@gmail.com>
2022-09-05 07:33:30 -0400

    Use prod url

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,7 +11,7 @@ import { ServiceProvider } from './contexts/services';
 // initialize dependencies
 const devUrl = `http://localhost:8000/api`;
 const prodUrl = `${window.location.origin}/api`;
-const eventHandler: EventHandler = initSocketIOEventHandler(io(devUrl));
+const eventHandler: EventHandler = initSocketIOEventHandler(io(prodUrl));
 const api: IApi = initApi(eventHandler);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
